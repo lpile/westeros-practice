571dbf3e9014640c5da5a8f86ddf2c78b6dd1142
lpile <logan.pile@gmail.com>
2019-07-19 10:29:33 -0600

    Add connection to API

--- app/facades/house_search_facade.rb
+++ app/facades/house_search_facade.rb
@@ -1,10 +1,12 @@
 class HouseSearchFacade
+  attr_reader :house
+
   def initialize(house)
     @house = house
   end
 
   def members
-
+    members = get_member_data(@house)
   end
 
   def member_count
@@ -12,6 +14,14 @@ class HouseSearchFacade
   end
 
   def conn
+    Faraday.new(url: 'http://westerosapi.herokuapp.com') do |faraday|
+      faraday.params['api_key'] = ENV['WESTEROS_API_KEY']
+      faraday.adapter Faraday.default_adapter
+    end
+  end
 
+  def get_member_data(house_name)
+    response = conn.get("/api/v1/house/#{house_name}")
+    JSON.parse(response.body, symbolize_names: true)[:data]
   end
 end
