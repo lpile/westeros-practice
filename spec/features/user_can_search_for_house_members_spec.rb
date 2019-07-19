2419423137762eda6f5c7a5b8d6c392fc44a0d9b
lpile <logan.pile@gmail.com>
2019-07-19 09:33:32 -0600

    Add user can search for house members spec

---
+++ spec/features/user_can_search_for_house_members_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+feature 'User can search for westeros house members' do
+  scenario 'by valid house name' do
+    # As a user
+    # When I visit "/"
+    visit '/'
+
+    # And I select "Greyjoy" from the dropdown
+    # And I click on "Get Members"
+    select "Greyjoy", from: :state
+    click_on "Get Members"
+
+    # Then my path should be "/search" with "house=greyjoy" in the parameters
+    expect(current_path).to eq("/search")
+    expect(current_url).to include("house=greyjoy")
+
+    # And I should see a message "7 Members"
+    expect(page).to have_content("7 Members")
+    # And I should see a name and id for each member.
+    expect(page).to have_all_of_selectors(".member_list-name", ".member_list-id")
+
+    # And I should see a list of the 7 members of House Greyjoy
+    expect(page).to have_css(".member_list", count: 7)
+  end
+end
